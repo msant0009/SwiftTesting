5f45bf8e6bfbab7946d73f35a180166ab5460abe
Mark Santoro <mark_santoro@comcast.net>
2024-09-10 17:09:42 -0700

    initial setup

---
+++ SwiftTesting/MathematicFunctions.swift
@@ -0,0 +1,27 @@
+//
+//  MathematicFunctions.swift
+//  SwiftTesting
+//
+//  Created by Mark Santoro on 9/10/24.
+//
+
+import Foundation
+
+class MathematicFunctions {
+    
+    func addIntegers(x: Int, y: Int) ->Int{
+        return x + y
+    }
+    
+    func multiplyIntegers(x: Int, y: Int) ->Int{
+        return x * y
+    }
+    
+    func divideIntegers(x: Int, y: Int) ->Int{
+        return x / y
+    }
+    
+    
+    
+    
+}
